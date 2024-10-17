60fb0a1c225a5e17b6fc305846f88793a5ee4055
AstronGaming <yashudkl@gmail.com>
2024-10-18 00:24:56 +0545

    Contact Updated

--- my-portfolio/src/components/Contact.js
+++ my-portfolio/src/components/Contact.js
@@ -1,11 +1,29 @@
 import React from 'react';
 import './Contact.css';
+
 const Contact = () => {
   return (
-    <section id="contact" className="p-4">
-      <h2 className="text-3xl font-bold mb-2">Contact Me</h2>
-      <p>Feel free to email me at: <a href="mailto:yashudkl@gmail.com" className="text-blue-500">yashudkl@gmail.com</a></p>
-    </section>
+    <div className="contact-container">
+      <h2>Contact Me</h2>
+      <p>If you'd like to get in touch, feel free to reach out through any of the platforms below:</p>
+      <ul className="contact-list">
+        <li>
+          <a href="mailto:yashudkl@gmail.com" target="_blank" rel="noopener noreferrer">
+            <i className="fas fa-envelope"></i>yashudkl@gmail.com
+          </a>
+        </li>
+        <li>
+          <a href="https://github.com/yashudkl" target="_blank" rel="noopener noreferrer">
+            <i className="fab fa-github"></i> GitHub
+          </a>
+        </li>
+        <li>
+          <a href="https://www.linkedin.com/in/yashwant-dhakal-225060327/" target="_blank" rel="noopener noreferrer">
+            <i className="fab fa-linkedin"></i> LinkedIn
+          </a>
+        </li>
+      </ul>
+    </div>
   );
 };
 
