4e5c1322fb4c423492077e3929b5c9f5bc356db1
yashudkl <yashudkl@gmail.com>
2024-11-02 23:01:56 +0545

    auto-typing

--- my-portfolio/src/components/About.js
+++ my-portfolio/src/components/About.js
@@ -1,12 +1,20 @@
 import React from 'react';
 import './About.css';
+import TypingEffect from 'react-typing-effect';
 const About = () => {
   return (
     <section id="about" className="p-4">
 <img 
 className="profile-image" src="/images/my-image.jpg" alt="me" />
 
-      <h2 className="text-3xl font-bold mb-2">Hello! I’m Yashwant,</h2>
+      <h2 className="text-3xl font-bold mb-2"> <TypingEffect
+          text={["Hello! I’m Yashwant,"]}
+          speed={100}
+          eraseSpeed={50}
+          eraseDelay={2000}
+          typingDelay={500}
+          cursorColor="#000"
+        /></h2>
       <p> 
  Computer Science student at Kathmandu University also a budding developer passionate about web development and currently learning it. 
 
